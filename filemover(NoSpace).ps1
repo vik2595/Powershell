8c6ed002ea8ea9d581602ae537a30f1834740910
vik2595 <47865384+vik2595@users.noreply.github.com>
2020-02-19 09:08:27 -0500

    Move files according to file name
    powershell script to move files in client folder according to file name.

---
+++ filemover(NoSpace).ps1
@@ -0,0 +1,16 @@
+﻿#Test
+#$files = (gci "C:\Users\acct\Desktop\Vikas Data\Test\*" -include ("*.doc","*.jpg","*.pdf","*.xlsx","*.xls","*.docx"))
+#$Dfolder = "C:\Users\acct\Desktop\Vikas Data\Test\client files\"
+
+
+#Real
+$files = (gci "C:\Users\acct\Dropbox\ORDER PROCESSING\TO BE PUSHED OUT - Donny\Adam please drop these into client files\Move\*" -include ("*.doc","*.jpg","*.pdf","*.xlsx","*.xls","*.docx"))
+$Dfolder = "C:\Users\acct\Dropbox\client files\"
+
+
+foreach($File in $files)
+    { 
+    $dest = $File.name.split('[ ]')[2] + ", " + $File.name.split('[ ]')[1] 
+    New-Item -Path $Dfolder -Name $dest -ItemType "directory"
+    Move-Item -Path "$File" -Destination "$Dfolder\$Dest";
+    }
---
+++ filemover.ps1
@@ -0,0 +1,8 @@
+﻿$files = (gci "C:\Users\acct\Dropbox\ORDER PROCESSING\TO BE PUSHED OUT - Donny\Adam please drop these into client files\Move\*" -include ("*.doc","*.pdf","*.xlsx","*.xls","*.docx"))
+$Dfolder = "C:\Users\acct\Dropbox\client files\"
+foreach($File in $files)
+    { 
+    $dest = $File.name.split('[ ]')[3] + ", " + $File.name.split('[ ]')[2] 
+    New-Item -Path $Dfolder -Name $dest -ItemType "directory"
+    Move-Item -Path "$File" -Destination "$Dfolder\$Dest";
+    }
